84f1b6054280be4ca7d18d5a6336bc05b4deabd3
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-07-06 20:36:44 -0400

    Activate

--- src/main/java/com/vandenbreemen/corpuscles/CorpusclesData.java
+++ src/main/java/com/vandenbreemen/corpuscles/CorpusclesData.java
@@ -10,4 +10,12 @@ public class CorpusclesData {
     public CorpusclesData(int height, int width) {
         data = new byte[height][width];
     }
+
+    public boolean activated(int alongHeight, int alongWidth) {
+        return (data[alongHeight][alongWidth] & (1 << 0)) != 0;
+    }
+
+    public void activate(int alongHeight, int alongWidth) {
+        data[alongHeight][alongWidth] |= (1 << 0);
+    }
 }
--- src/test/java/com/vandenbreemen/corpuscles/CorpusclesDataTest.java
+++ src/test/java/com/vandenbreemen/corpuscles/CorpusclesDataTest.java
@@ -11,4 +11,21 @@ public class CorpusclesDataTest {
         CorpusclesData data = new CorpusclesData(10, 10);
     }
 
+    @Test
+    public void testGetActivatedIsFalseByDefault() {
+        CorpusclesData data = new CorpusclesData(10, 10);
+        for(int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                assertFalse(data.activated(i,j));
+            }
+        }
+    }
+
+    @Test
+    public void testActivate() {
+        CorpusclesData data = new CorpusclesData(10, 10);
+        data.activate(2,1);
+        assertTrue(data.activated(2, 1));
+    }
+
 }
